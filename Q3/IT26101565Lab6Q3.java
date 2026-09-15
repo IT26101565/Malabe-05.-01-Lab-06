1283d264f22f774b87c4c6ebdf0d0cdd10a05b1c
IT26101565 <it26101565@my.sliit.lk>
2026-09-15 21:41:48 +0530

    IT26101565Lab6Q3.java

---
+++ Q3/IT26101565Lab6Q3.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class IT26101565Lab6Q3 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter positive integers (terminate input with -99):");
+        
+        int count = 0;
+        double sumSquares = 0;
+        
+        while (true) {
+            System.out.print("Enter a number: ");
+            int num = scanner.nextInt();
+            
+            if (num == -99) {
+                break;
+            }
+            
+            if (num < 0) {
+                System.out.println("Invalid input. Please enter a positive integer or -99 to terminate");
+                continue;
+            }
+            
+            sumSquares += (num * num);
+            count++;
+        }
+        
+        if (count > 0) {
+            double rms = Math.sqrt(sumSquares / count);
+            System.out.println("The Root Mean Square (RMS) is: " + rms);
+        } else {
+            System.out.println("No numbers were entered.");
+        }
+        
+        scanner.close();
+    }
+}
