d18e64ce5f0a3bc02e9b5be90572c667ae842046
IT26101565 <it26101565@my.sliit.lk>
2026-09-15 21:41:10 +0530

    IT26101565Lab6Q2A.java, IT26101565Lab6Q2B.java, IT26101565Lab6Q2C.java

---
+++ Q2/IT26101565Lab6Q2B.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class IT26101565Lab6Q2B {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Please enter 10 numbers:");
+        
+        System.out.println("Enter number 1:");
+        int n1 = scanner.nextInt();
+        
+        System.out.println("Enter number 2:");
+        int n2 = scanner.nextInt();
+        
+        System.out.println("Enter number 3:");
+        int n3 = scanner.nextInt();
+        
+        System.out.println("Enter number 4:");
+        int n4 = scanner.nextInt();
+        
+        System.out.println("Enter number 5:");
+        int n5 = scanner.nextInt();
+        
+        System.out.println("Enter number 6:");
+        int n6 = scanner.nextInt();
+        
+        System.out.println("Enter number 7:");
+        int n7 = scanner.nextInt();
+        
+        System.out.println("Enter number 8:");
+        int n8 = scanner.nextInt();
+        
+        System.out.println("Enter number 9:");
+        int n9 = scanner.nextInt();
+        
+        System.out.println("Enter number 10:");
+        int n10 = scanner.nextInt();
+        
+        System.out.println("The numbers you entered are:");
+        System.out.println(n1 + " " + n2 + " " + n3 + " " + n4 + " " + n5 + " " + n6 + " " + n7 + " " + n8 + " " + n9 + " " + n10);
+        
+        scanner.close();
+    }
+}
